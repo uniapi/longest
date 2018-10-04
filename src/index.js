d370e07af0da0a62f263ddefb66027a83e6cbbe8
Ali Muhammed <uniapi@outlook.com>
2018-10-04 08:01:13 +0300

    Added [src/index.js] - added the implementation of longestConsecutiveSequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+/*************************************************************************
+                          Written by Ali Muhammed
+                            uniapi@outlook.com
+                              October 4, 2018
+*************************************************************************/
+
+module.exports = function longestConsecutiveLength(seq) {
+	const len = seq.length;
+	let long = 1;
+	let cnt = 1;
+	let diff;
+	if (!len)
+		return 0;
+	seq.sort((a, b) => a - b);
+	for (let i=1; i < len; i++) {
+		diff = seq[i] - seq[i-1];
+		if (diff === 1)
+			cnt++;
+		else if (diff > 0) {
+			if (long < cnt) {
+				long = cnt;
+				if (len - i < long)
+					break;
+			}
+			cnt = 1;
+		}
+	}
+	return cnt === len ? cnt : long < cnt ? cnt : long;
 }
